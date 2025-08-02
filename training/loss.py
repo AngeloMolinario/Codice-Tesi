a4a79024af1bbc1e73e1df29a2995d431da1d334
AngeloMolinario <angelomolinario75@gmail.com>
2025-08-02 15:33:57 +0200

    added the weighted version of the AgeOrdinalLoss to be tested

--- training/loss.py
+++ training/loss.py
@@ -22,6 +22,43 @@ class AgeOrdinalLoss():
             return loss, predicted_label
         return loss
     
+import torch
+import torch.nn as nn
+
+class _WeightedAgeOrdinalLoss():
+    # TODO: to test
+    def __init__(self, num_classes, ordinal_loss='mae', class_frequencies=None):
+        self.num_classes = num_classes
+        self.ordinal_loss = nn.L1Loss(reduction='none') if ordinal_loss == 'mae' else nn.MSELoss(reduction='none')
+        self.classes_range = torch.arange(num_classes).float()
+        
+        if class_frequencies is not None:
+            self.class_weights = self.calculate_class_weights(class_frequencies)
+        else:
+            self.class_weights = torch.ones(num_classes)
+
+    def calculate_class_weights(self, class_frequencies):
+        total_samples = sum(class_frequencies)
+        class_weights = [total_samples / (self.num_classes * f) for f in class_frequencies]
+        return torch.tensor(class_weights).float()
+
+    def __call__(self, logit, true_labels, return_predicted_label=False):
+        probabilities = torch.softmax(logit, dim=1)
+        expected_value = torch.sum(probabilities * self.classes_range.unsqueeze(0), dim=1).float()
+        predicted_label = torch.round(expected_value).long()
+
+        # Calcola la perdita per esempio
+        losses = self.ordinal_loss(expected_value, true_labels.float())
+
+        # Applica il bilanciamento ponderando la perdita
+        weighted_losses = losses * self.class_weights[true_labels.long()]
+
+        # Calcola la media ponderata
+        loss = weighted_losses.mean()
+
+        if return_predicted_label:
+            return loss, predicted_label
+        return loss
 
 class CrossEntropyLoss():
     def __init__(self):
