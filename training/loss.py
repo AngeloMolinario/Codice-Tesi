59d408df1a0bf57e87fd2be10de1a7a8e6f3d56e
AngeloMolinario <angelomolinario75@gmail.com>
2025-08-19 12:58:34 +0200

    1

--- training/loss.py
+++ training/loss.py
@@ -22,11 +22,11 @@ class OrdinalPeakedCELoss(nn.Module):
     - widths (opz.)     : pesi per EMD^2 quando i bin hanno ampiezze diverse
     """
     def __init__(self, num_classes, weights=None, widths=[3, 7, 10, 10, 10, 10, 10, 10, 25],
-                 alpha=0.5,
-                 w_far=1.0,      delta_far=0.15,
-                 w_lpeak=1.0,    prob_margin=0.2,
-                 w_emd=1.0,
-                 ce_weight=0.0,
+                 alpha=1.0,
+                 w_far=1.0,      delta_far=0.01,
+                 w_lpeak=4.0,    prob_margin=0.2,
+                 w_emd=3.5,
+                 ce_weight=0.5,
                  eps=1e-8):
         super().__init__()
         self.num_classes = int(num_classes)
