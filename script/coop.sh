1f59100e27fcde7d1da9b51555a7103e169c7c69
Angelo Molinario <angelomolinario75@gmail.com>
2025-09-13 17:29:40 +0000

    config

--- script/coop.sh
+++ script/coop.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-python3 coop_train.py config/coop/PE_joint.json
-python3 coop_train.py config/coop/PE_coop.json
-python3 coop_train.py config/coop/PE_vpt.json
-
 python3 coop_train.py config/coop/SL_joint.json
 python3 coop_train.py config/coop/SL_coop.json
 python3 coop_train.py config/coop/SL_vpt.json
+python3 coop_train.py config/coop/SL_vpt_ft.json
